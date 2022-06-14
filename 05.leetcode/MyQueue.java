c05da1382271041405a8cbd2f5ac10a01f486c0d
niusb <34199781>
2022-06-14 10:11:56 +0800

    【459】 重复的子字符串 && 队列、栈的互相实现@niusb

---
+++ 05.leetcode/MyQueue.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+//232. 用栈实现队列
+class MyQueue {
+
+    Stack<Integer> stackIn;
+    Stack<Integer> stackOut;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stackIn = new Stack<>(); // 负责进栈
+        stackOut = new Stack<>(); // 负责出栈
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        dumpstackIn();
+        return stackOut.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        dumpstackIn();
+        return stackOut.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+    // 如果stackOut为空，那么将stackIn中的元素全部放到stackOut中
+    private void dumpstackIn(){
+        if (!stackOut.isEmpty()) return;
+        while (!stackIn.isEmpty()){
+            stackOut.push(stackIn.pop());
+        }
+    }
+}

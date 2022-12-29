b8fab5763101b3a7ee6795627ccd00e62ff1f13c
niusb <34199781>
2022-12-29 11:20:56 +0800

    不含有重复字符的 最长子串 的长度&&简历更新@niusb

---
+++ 01.leetcode/q3.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+public class q3 {
+//    给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+//
+//             
+//
+//    示例 1:
+//
+//    输入: s = "abcabcbb"
+//    输出: 3
+//    解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+//    示例 2:
+//
+//    输入: s = "bbbbb"
+//    输出: 1
+//    解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+//    示例 3:
+//
+//    输入: s = "pwwkew"
+//    输出: 3
+//    解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//                 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        q3 q3 = new q3();
+        System.out.println(q3.lengthOfLongestSubstring(s));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i ++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-left+1);
+        }
+        return max;
+
+    }
+}
